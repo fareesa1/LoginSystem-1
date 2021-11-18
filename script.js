356375f045841b18fad4a8040b32474d98b8402c
fareesa_mas <30589399+fareesa1@users.noreply.github.com>
2021-11-18 15:11:43 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var input_description;
 var x1,y1;
 var currentUserUid=""
 var postTitle=""
+var email="";
 
 const firebaseConfig = {
    apiKey: "AIzaSyDyqYOnUsPMdmth2HQ3cOLvq1-VoBCO6aA",
@@ -19,10 +20,6 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 db = firebase.database()
-
-
-window.onload = function() {
-  }
 
 function login(){
     x= document.getElementById("input1").value 
@@ -59,7 +56,9 @@ function loginUser(email, password) {
             // alert('Successfully authenticated!');
         currentUserUid=cred.user.uid
         db.ref("users/").update({email:cred.user.email,uid:cred.user.uid})
-        console.log(cred.user.uid);
+        console.log(cred.user.email);
+        var email=cred.user.email
+        localStorage.setItem('email', JSON.stringify(email));
             window.location.href = 'createpost.html';
             // direct to logic success page
         }).catch(err => {
@@ -68,36 +67,82 @@ function loginUser(email, password) {
          
 }
 
+var check_user=document.getElementById("check_user")
+console.log(check_user);
+window.addEventListener("load",checkUser)
+function checkUser() {
+    console.log("i am in check user");
+    email= JSON.parse(localStorage.getItem("email"));
+    console.log(email);
+    console.log(check_user);
+    if(email){
+        document.getElementById("logout").innerHTML=`<button onclick="logout()">logout</button>`
+        var div1=document.createElement("div")
+        div1.classList.add("form-container")
+        div1.innerHTML=`<input type="title" id="input_title" placeholder="Enter title for the post">
+        <textarea rows = "5" cols = "40" id="input_description" name = "description">
+         </textarea>
+        <button style="border-radius: 20px;" type="button" onclick="addUserData()"  >Create Post</button>`
+        check_user.appendChild(div1)
+        console.log(check_user);
+        window.scrollBy(0, 50);
+}else{
+
+ var a=document.createElement("a")
+a.href="index.html"
+a.textContent="please login"  
+ check_user.appendChild(a)
+ console.log(check_user);
+}
+}
 
 function addUserData(){
     var rand = Math.round(Math.random() *60) + Math.round(Math.random() * 800)
     // rand = currentUserUid
     input_title = document.getElementById("input_title").value
     input_description = document.getElementById("input_description").value
-    db.ref("posts/"+rand + "/").update({title:input_title, description:input_description, timeStamp:new Date().getTime()})
+    db.ref("posts/"+rand + "/").update({title:input_title, description:input_description, timeStamp:new Date().getTime(), email:email})
     // console.log(input_title)
     window.location.href = 'feed.html';
-
 }
 
 function readUserData(){
     db.ref("posts/").on("value", (data)=>{
         var readPost = data.val()
+        
         console.log(readPost)
-
-var arr = Object.keys(readPost)
-console.log(arr[0])
-console.log(readPost[arr[0].toString()].title)
-postTitle = readPost[arr[2].toString()].title
-document.getElementById("post_title").innerHTML = postTitle
-
+        var arr = Object.keys(readPost)
+        var length=arr.length;
+        console.log(length);
+        var feed=document.getElementById("feed")
+        console.log(feed);
+        for(var i=0; i<length-3; i++){
+            console.log(readPost[arr[i].toString()].title)
+            postTitle = readPost[arr[i].toString()].title
+            var div=document.createElement("div")
+            div.innerHTML=`<div class="card">
+            <img src="https://picsum.photos/200/300/" width="350" height="300">
+            <a href="readPost.html" onclick="redirect(1)"  >
+                <h2 id="post_title" >${postTitle}</h2>
+            </a>
+            <p>RASHMI BANSAL</p>
+        </div>`
+        console.log(div);
+      
+        feed.appendChild(div); 
+        }
+       
+       
     })
 }
 
-// function readIds(){
+function currentUserInfo(){
 
-// }
+}
 
-
+function logout() {
+    localStorage.removeItem("email"); 
+    location.reload(); 
+}
 
 
